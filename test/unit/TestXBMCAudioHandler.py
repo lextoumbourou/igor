aba943539b21cd92473a3a03e90a5a8f00843b18
Lex Toumbourou <lextoumbourou@gmail.com>
2014-05-12 22:47:01 +1000

    Split JSON into separate module

--- test/unit/TestXBMCAudioHandler.py
+++ test/unit/TestXBMCAudioHandler.py
@@ -3,22 +3,16 @@ from mock import MagicMock
 
 from lib import XBMCAudioHandler
 
-class TestXBMCAudioHandler(TestCase):
-    ARTIST_JSON = {
-        'id': 1,
-        'jsonrpc': 2.0,
-        'results': {'artists': [
-            {'label': 'James Blake', 'artistid': 1},
-            {'label': 'Mark Morrison', 'artistid': 2}
-        ]}
-    }
+import mock_json
 
+
+class TestXBMCAudioHandler(TestCase):
     def setUp(self):
         self.xbmc_audio = XBMCAudioHandler(MagicMock())
 
     def test_find_artist_when_requested(self):
         (self.xbmc_audio
-            .conn.AudioLibrary.GetArtists.return_value) = self.ARTIST_JSON
+            .conn.AudioLibrary.GetArtists.return_value) = mock_json.ARTIST_JSON
         result = self.xbmc_audio.find_closest_artist_match(
             'james blake')
         assert result['label'] == 'James Blake'
---
+++ test/unit/mock_json.py
@@ -0,0 +1,21 @@
+ARTIST_JSON = {
+    'id': 1,
+    'jsonrpc': 2.0,
+    'results': {
+        'artists': [
+            {'label': 'James Blake', 'artistid': 1},
+            {'label': 'Mark Morrison', 'artistid': 2},
+        ]
+    }
+}
+
+SONG_JSON = {
+    'id': 6,
+    'jsonrpc': u'2.0',
+    'result': {
+        'songs': [
+            {'label': 'Song 1', 'songid': 8249},
+            {'label': 'Song 2', 'songid': 8780},
+        ]
+    }
+}
