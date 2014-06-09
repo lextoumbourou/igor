acf2a79717ebe812ede9d7a248a7b826d9c7fdbd
Lex Toumbourou <lextoumbourou@gmail.com>
2014-06-09 12:19:12 +1000

    Fixed controller deps spec

--- test/js/unit/controllerSpec.js
+++ test/js/unit/controllerSpec.js
@@ -6,7 +6,10 @@ describe('igor controllers', function() {
 
       it('should have the correct initial title', inject(function($controller) {
           var scope = {},
-              ctrl = $controller('MainController', {$scope:scope});
+              ctrl = $controller(
+                'MainController',
+                {$scope:scope, $http:{}, witService:{}, xbmcRouter:{}, speech:{}}
+                );
 
           expect(scope.title).toBe('Why Not?');
       }));
