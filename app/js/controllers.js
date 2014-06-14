31f612f9f11194496c825c3160d2bd5a80465080
Lex Toumbourou <lextoumbourou@gmail.com>
2014-06-15 02:06:32 +1000

    Rewrite Speech Recognition handler (WIP)

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,12 +7,14 @@ angular.module('igor.controllers', ['igor.services', 'xbmc.services'])
     'witService',
     'router',
     'speech',
-    function ($scope, $http, witService, xbmcRouter, speech) {
+    'speechListen',
+    function ($scope, $http, witService, xbmcRouter, speech, speechListen) {
 
       $scope.title = 'Why Not?';
       $scope.body = null;
       $scope.subtitle = 'Tell me what you want';
       $scope.potential = 'Test';
+      $scope.isListening = false;
 
       $scope.lastRecognition = 0;
       $scope.timeBetweenCommands = 3000;
@@ -22,48 +24,33 @@ angular.module('igor.controllers', ['igor.services', 'xbmc.services'])
 
       $scope.updateAndPlay = function(msg) {
         $scope.subtitle = msg;
+        speechListen.stop();
         speech.say(msg);
       }
 
-      $scope.isNewCommand = function() {
-        var tmpTime = new Date().getTime();
-        if (tmpTime - $scope.lastRecognition > $scope.timeBetweenCommands) {
-          $scope.lastRecognition = tmpTime;
-          $scope.finalTranscript = '';
+      speechListen.onresult = function(event) {
+        var result = '';
+        for (var i = event.resultIndex; i < event.results.length; i++) {
+          $scope.$apply(function() {
+            $scope.title = event.results[i][0].transcript;
+          });
         };
       };
 
-      $scope.rec = new webkitSpeechRecognition();
-      $scope.rec.continuous = true;
-      $scope.rec.lang = 'en-US';
-      $scope.rec.onresult = function(event) {
-        $scope.isNewCommand();
-        $scope.findAndUpdateResults(event);
+      speechListen.onstart = function() {
+        $scope.isListening = true;
       };
 
-      $scope.rec.onend = function(evt) {
+      speechListen.onend = function() {
+        $scope.isListening = false;
+        if ($scope.title) {
+          $scope.getMeaningOfTitle();
+        };
       };
 
-      $scope.findAndUpdateResults = function(event) {
-        $scope.$apply(function() {
-          finalTranscript = '';
-          for (var i = event.resultIndex; i < event.results.length; ++i) {
-            if (event.results[i].isFinal) {
-              finalTranscript += event.results[i][0].transcript;
-            } else {
-              $scope.title += event.results[i][0].transcript;
-            }
-          };
-
-          $scope.title = finalTranscript;
-        });
-
-        $scope.getMeaningOfTitle();
+      speechListen.onerror = function(error) {
+        console.log('speech error');
       };
-
-      $scope.handleResults = function(data) {
-        
-      }
 
       $scope.clearLastCommand = function() {
         var tmpTime = new Date().getTime();
@@ -75,7 +62,6 @@ angular.module('igor.controllers', ['igor.services', 'xbmc.services'])
       $scope.getMeaningOfTitle = function() {
         witService.getMessage($scope.title).
           success(function(data, status) {
-
             var intent = data.outcome.intent;
             if (intent in xbmcRouter) {
               xbmcRouter[intent](data.outcome).then(function(result) {
@@ -89,16 +75,17 @@ angular.module('igor.controllers', ['igor.services', 'xbmc.services'])
               $scope.updateAndPlay('Not really sure what you mean?');
             }
           }).
-          error(function() {
+          error(function(data, status) {
             $scope.updateAndPlay('Something is wrong with the server');
           });
       };
 
-      $scope.startListening = function() {
-        $scope.rec.start();
-      };
-
-      $scope.stopListening = function() {
-        $scope.rec.stop();
+      $scope.toggleListening = function() {
+        if ($scope.isListening) {
+          speechListen.stop();
+        }
+        else if (!$scope.isListening) {
+          speechListen.start();
+        };
       };
   }]);
