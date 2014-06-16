fdf043fc90be96fd9a9b828cf028464a1235cab4
Lex Toumbourou <lextoumbourou@gmail.com>
2014-06-16 23:22:05 +1000

    removed pointless controller test

--- test/unit/controllerSpec.js
+++ test/unit/controllerSpec.js
@@ -1,25 +1,9 @@
 describe('igor controllers', function() {
 
   describe('igor MainController', function() {
-
-      beforeEach(function() {
-        angular.mock.module('igor')
-        config = {
-          xbmc: {
-            ip: '127.0.0.1', port: '9090'
-          }
-        };
-
-      });
-
-      it('should have the correct initial title', inject(function($controller) {
-          var scope = {},
-              ctrl = $controller(
-                'MainController',
-                {$scope:scope, $http:{}, witService:{}, xbmcRouter:{}, speech:{}}
-                );
-
-          expect(scope.title).toBe('Why Not?');
-      }));
+    beforeEach(function() {
+      angular.mock.module('igor');
+    });
   });
+
 });
