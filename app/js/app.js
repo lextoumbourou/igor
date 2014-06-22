4d5186a037720d834d1a0ea6d9f38df5c9e5fe70
Lex Toumbourou <lextoumbourou@gmail.com>
2014-06-22 16:08:47 +1000

    Added temporary solution to auto scale the textarea

--- app/js/app.js
+++ app/js/app.js
@@ -23,3 +23,9 @@ app.config(['$routeProvider', function($routeProvider) {
       redirectTo: '/result'
     });
 }]);
+
+var autoGrow = function (oField) {
+  if (oField.scrollHeight > oField.clientHeight) {
+    oField.style.height = oField.scrollHeight + "px";
+  }
+}
